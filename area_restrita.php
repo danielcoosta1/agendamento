f3e1022876eb290a3c462640403f53d10711b3f4
Daniel Costa <daniel.dc697@gmail.com>
2025-11-17 23:22:21 -0300

    Arquivo para verificar a session

---
+++ area_restrita.php
@@ -0,0 +1,41 @@
+<?php
+// 1. INICIAR A SESSÃO (Aula 9, slide 26) 
+// TEMOS de iniciar a sessão em TODAS as páginas que
+// precisam de saber se o utilizador está logado.
+session_start();
+
+// 2. VERIFICAR SE O UTILIZADOR ESTÁ LOGADO (Aula 9, slide 33) 
+// Verificamos se a "chave" 'usuario_logado' que criámos em 'verificar_login.php'
+// existe e não está vazia na $_SESSION.
+if (isset($_SESSION['usuario_logado']) && !empty($_SESSION['usuario_logado'])) {
+    
+    // Se sim, o utilizador está logado.
+    // Guardamos o nome dele numa variável para facilitar.
+    $nome_usuario = $_SESSION['usuario_logado'];
+
+} else {
+    // Se não, o utilizador NÃO está logado.
+    // Redirecionamos ele de volta para a página de login (Aula 9, slide 33) 
+    header('Location: login.php');
+    exit;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Área Restrita</title>
+    </head>
+<body>
+    <h1>Bem-vindo à Área Restrita, <?php echo $nome_usuario; ?>!</h1>
+    
+    <p>Esta é a página principal do seu sistema de agendamento.</p>
+    <p>Só utilizadores logados podem ver isto.</p>
+
+    <br>
+    
+    <a href="logout.php">Sair (Logout)</a>
+</body>
+</html>
